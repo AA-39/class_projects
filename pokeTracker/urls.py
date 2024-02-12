5beb291aaa58d7d96003ff59223e271bdf714bba
AA-39 <aackerman212@gmail.com>
2024-02-12 08:07:43 -0500

    Add Files

--- pokeTracker/urls.py
+++ pokeTracker/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('pokedex/', views.pokedex, name='pokedex'),
+    path('shinydex/', views.shinydex, name='shinydex')
 ]
--- pokeTracker/views.py
+++ pokeTracker/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pokeTracker/index.html')
+
+def login(request):
+    return render(request, 'pokeTracker/login.html')
+
+def pokedex(request):
+    return render(request, 'pokeTracker/pokedex.html')
+
+def shinydex(request):
+    return render(request, 'pokeTracker/shinydex.html')
