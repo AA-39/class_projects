b64549ba76597936c08a065f097f84e05d1b6f19
AA-39 <aackerman212@gmail.com>
2024-02-13 13:00:59 -0500

    Changes

--- pokeTracker/views.py
+++ pokeTracker/views.py
@@ -9,5 +9,9 @@ def login(request):
 def pokedex(request):
     return render(request, 'pokeTracker/pokedex.html')
 
+def checkbox_form(request):
+    image_numbers = range(1, 31)  # Assuming you have images named 1.jpg, 2.jpg, ..., 30.jpg
+    return render(request, 'pokeTracker/pokedex.html', {'image_numbers': image_numbers})
+
 def shinydex(request):
     return render(request, 'pokeTracker/shinydex.html')
