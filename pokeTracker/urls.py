7ea201df8b05bc2030c25014a0642de3746335da
AA-39 <aackerman212@gmail.com>
2024-03-16 19:25:33 -0400

    Sprint 2 Updates

--- pokeTracker/urls.py
+++ pokeTracker/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
     path('pokedex/', views.pokedex, name='pokedex'),
     path('shinydex/', views.shinydex, name='shinydex')
 ]
--- pokeTracker/views.py
+++ pokeTracker/views.py
@@ -1,14 +1,39 @@
 ## class_projects/pokeTracker/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 from .models import Pokemon
+from .forms import RegistrationForm
 
 def index(request):
-    return render(request, 'pokeTracker/index.html')
+    return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'pokeTracker/login.html')
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('index')
+    else:
+        form = AuthenticationForm(request)
 
+    return render(request, 'authentication/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'authentication/register.html', {'form': form})
+
+@login_required(login_url='login')
 def pokedex(request):
     regions = ['Kanto', 'Johto', 'Hoenn', 'Sinnoh', 'Unova', 'Kalos', 'Alola', 'Galar', 'Paldea']
     region_data = []
@@ -21,8 +46,9 @@ def pokedex(request):
         }
         region_data.append(region_info)
 
-    return render(request, 'pokeTracker/pokedex.html', {'regions': region_data})
+    return render(request, 'pokedex.html', {'regions': region_data})
 
+@login_required(login_url='login')
 def shinydex(request):
     regions = ['Kanto', 'Johto', 'Hoenn', 'Sinnoh', 'Unova', 'Kalos', 'Alola', 'Galar', 'Paldea']
     region_data = []
@@ -35,4 +61,4 @@ def shinydex(request):
         }
         region_data.append(region_info)
 
-    return render(request, 'pokeTracker/shinydex.html', {'regions': region_data})
+    return render(request, 'shinydex.html', {'regions': region_data})
